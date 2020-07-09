4f86abf08463968e13d0a4d22b684d06eeec7cf2
anupongiv4 <anuponglive@gmail.com>
2020-07-09 22:19:12 +0700

    Updates

---
+++ precache-manifest.95126bc67de5418a349bd43ba8faedcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fc1daf7ffbefbfc8aef5811b88a9e54",
+    "url": "/mini-weather-app/index.html"
+  },
+  {
+    "revision": "74915839aea0731267b1",
+    "url": "/mini-weather-app/static/css/main.9484f875.chunk.css"
+  },
+  {
+    "revision": "ad5e60885d0dbc786b66",
+    "url": "/mini-weather-app/static/js/2.de9a5ad0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mini-weather-app/static/js/2.de9a5ad0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74915839aea0731267b1",
+    "url": "/mini-weather-app/static/js/main.ff6c647e.chunk.js"
+  },
+  {
+    "revision": "c3222523668d072522cb",
+    "url": "/mini-weather-app/static/js/runtime-main.06a8dd91.js"
+  }
+]);
